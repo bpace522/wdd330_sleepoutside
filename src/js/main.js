35e65a1e9ea47e21ffde5af8946571de1df2c3d1
gianvin <ginamangahassanchezong@gmail.com>
2025-11-08 22:13:02 +0800

    updated the main script to import the cart module and write the instance for cart

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+import { Cart } from "./cart.mjs"
 import { ProductData } from "./ProductData.mjs";
 import { ProductList } from ".ProductList.mjs"
 
@@ -7,5 +8,6 @@ const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
 
 const productList = new ProductList("tents, dataSource, element")
-
+const cart = new Cart();
 productList.init();
+cart.init();
