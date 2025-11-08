47db43ff6666dfc9d1ab81d333e2695c5350110b
gianvin <ginamangahassanchezong@gmail.com>
2025-11-08 22:00:37 +0800

    edited the cart.mjs using the init method

--- src/js/cart.mjs
+++ src/js/cart.mjs
@@ -1,40 +1,47 @@
-import { getLocalStorage, setLocalStorage } from ".utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
-export function renderCart() {
-    const cartContainer = document.querySelector(".cart-list");
-    const cartItems = getLocalStorage("so-cart") || [];
+export default class Cart {
+    constructor() {
+        this.cartContainer = document.querySelector(".cart-list");
+    }
 
-    if (!cartContainer) return;
+    init() {
+        this.renderCart();
+    }
 
-    cartContainer.innerHTML = cartItems.map(item =>
+    renderCart() {
+        const cartItems = getLocalStorage("so-cart") || [];
+        if (!this.cartContainer) {
+            console.error("Cart container not found");
+            return;
+        }
+        this.cartContainer.innerHTML = cartItems.map(item => `
         <li class="cart-item relative border rounded-lg p-4 bg-white shadow-sm">
         //Remove (x) icon
-            <button class="remove-btn absolute top-2 righ-2 text-gray-400 hover:text-red-500 text-lg"
+            <button class="remove-btn absolute top-2 right-2 text-gray-400 hover:text-red-500 text-lg"
                 data-id="${item.Id}" aria-label="Remove item">&times;</button>
 
             <div class="flex items-center gap-3">
-                <img src="${item.Image}" alt="${item.Name}" class="w=16 h=16 object-cover rounded">
+                <img src="${item.Image}" alt="${item.Name}" class="w-16 h-16 object-cover rounded">
                     <div>
                         <p class="font-medium">${item.Name}</p>
                         <p class="text-sm text-gray-600">$${item.FinalPrice}</p>
                     </div>
-                </img>
             </div>
-        </li>
-    ).join('');
+        </li>`
+        ).join('');
 
-    //event listener for all remove buttons
-    document.querySelectorAll(".remove-btn").forEach(btn => {
-        btn.addEventListener("click", () => {
-            removeFromCart(btn.dataset.id);
+        //event listener for all remove buttons
+        document.querySelectorAll(".remove-btn").forEach((btn) => {
+            btn.addEventListener("click", () => {
+                this.removeFromCart(btn.dataset.id);
+            });
         });
-    });
-
-}
-
-export function removeFromCart(productId) {
-    let cart = getLocalStorage("so-cart") || [];
-    const updatedCart = cart.filter(item => item.Id != productId);
-    setLocalStorage("so-cart", updatedCart)
-    renderCart();
+    }
+    removeFromCart(productId) {
+        let cart = getLocalStorage("so-cart") || [];
+        const updatedCart = cart.filter(item => item.Id != productId);
+        setLocalStorage("so-cart", updatedCart)
+        renderCart();
+    }
 }
