b229885bf24db3604cd2ac35b29d64cc55ef4e86
bpace522 <pacebc522@gmail.com>
2025-11-08 20:44:11 -0700

    cart remove button cart.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,8 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
-const productList = new ProductList("Tents", dataSource, element);
-const dataSource = new ProductData("tents");
-
 const element = document.querySelector(".product-list");
+const dataSource = new ProductData("tents");
+const productList = new ProductList("Tents", dataSource, element);
 
 productList.init();
