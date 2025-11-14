5c7dbbe9279041d0a0aa8bfeb4bb0f6d8c5075cf
gianvin <ginamangahassanchezong@gmail.com>
2025-11-14 10:08:51 +0800

    edited the utils.mjs as compare to the sample soulution

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -38,3 +38,26 @@ export function renderListWithTemplate(template, parentElement, list, position =
   }
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if (callback) {
+    callback(data);
+  }
+}
+
+async function loadtemplate(path) {
+  const res = await fetch(path);
+  const template = await renderListWithTemplate.text();
+  return template;
+}
+
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadtemplate("../partials/header.html");
+  const footerTemplate = await loadtemplate("../partials/footer.html");
+
+  const headerElement = document.querySelector("#main-header");
+  const footerElement = document.querySelector("#main-footer");
+
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement)
+}
