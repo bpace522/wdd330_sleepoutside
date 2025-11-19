ec1ba1a0d1210513b4f52f34296030c3956d7a72
gianvin <ginamangahassanchezong@gmail.com>
2025-11-19 09:42:30 +0800

    write the code for calculation of of tax rate, shipping fee and total amount of the order.

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -11,20 +11,39 @@ export default class CheckoutProcess {
     init() {
         this.list = getLocalStorage(this.key);
         this.calculateItemSummary();
+        this.calculateItemSubTotal();
+        this.calculateOrderTotal
     }
 
     calculateItemSubTotal() {
         //calculate and display the total dollar amount of the items
+        this.itemTotal = this.list.reduce((sum, item) => {
+            return sum + (item.price * item.quantity);
+        }, 0);
+
+        const subTotal = document.querySelector(`${this.outputSelector} #subTotal`);
+        subTotal.innerText = `$${this.itemTotal.toFixed(2)}`;
     }
 
     calculateOrderTotal() {
         //calculate the tax and shipping amounts. Add those to the cart total to figure out the order total
-        this.tax = (this.itemTotal ...)
-        this.shipping =
-            this.orderTotal =
+        const taxRate = 0.06
 
-            // display the totals
-            this.displayOrderTotal();
+        const totalItems = this.list.reduce((sum, item) => sum + item.quantity, 0);
+
+        if (totalItems <= 0) {
+            this.shipping = 0
+        } else {
+            const firstItemShipping = 10.00;
+            const additionalItemShipping = 2.00;
+            this.shipping = firstItemShipping + ((totalItems - 1) * additionalItemShipping);
+        }
+        this.tax = this.itemTotal * taxRate
+
+        this.orderTotal = this.itemTotal + this.tax + this.shipping;
+
+        // display the totals
+        this.displayOrderTotals();
     }
 
     displayOrderTotals() {
@@ -33,5 +52,10 @@ export default class CheckoutProcess {
 
         tax.innerText = `$${this.tax.toFixed(2)}`;
 
+        const shipping = document.querySelector(`${this.outputSelector} #shipping`);
+        shipping.innerText = `$${this.orderTotal.toFixed(2)}`;
+
+        const total = document.querySelector(`${this.outputSelector} #total`);
+        total.innerText = `$${this.orderTotal.toFixed(2)}`;
     }
 }
