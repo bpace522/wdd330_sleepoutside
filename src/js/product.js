8f7445b99c76e1e01808cce3d44ea5f00b909e6f
gianvin <ginamangahassanchezong@gmail.com>
2025-11-21 22:23:58 +0800

    edited the product,js as compare to sample solution

--- src/js/product.js
+++ src/js/product.js
@@ -1,21 +1,11 @@
-import { getLocalStorage, setLocalStorage, getParam, loadHeaderFooter } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductDetails from "./ProductDetails.mjs";
-import ProductData from "./ProductData.mjs";
 
-const dataSource = new ProductData("tents");
+loadHeaderFooter();
 
-function addProductToCart(product) {
-    const cartItems = getLocalStorage("so-cart") || [];
-    cartItems.push(product);
-    setLocalStorage("so-cart", cartItems);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-    const product = await dataSource.findProductById(e.target.dataset.id);
-    addProductToCart(product);
-}
+const dataSource = new ExternalServices("tents");
+const productID = getParam("product");
 
-// // add listener to Add to Cart button
-// document
-//   .getElementById("addToCart")
-//   .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productID, dataSource);
+product.init();
